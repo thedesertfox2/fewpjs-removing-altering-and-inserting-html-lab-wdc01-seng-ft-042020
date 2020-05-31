53e034cf4468b4924b56b373f1a11e79cf39105c
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-05-31 19:31:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove('main')
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+
+
+
